14876353c4d5b43f5292d4d4ae8c9b4f1dd875bf
PhilRanzato <philranzato@gmail.com>
2021-01-05 19:45:18 +0100

    Adds subcommands and change command with dash inside

--- cli/cmd/subcmdtest.go
+++ cli/cmd/subcmdtest.go
@@ -21,9 +21,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// connTestCmd represents the connTest command
-var connTestCmd = &cobra.Command{
-	Use:   "conn-test",
+// subcmdtestCmd represents the subcmdtest command
+var subcmdtestCmd = &cobra.Command{
+	Use:   "subcmdtest",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,20 +32,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("connTest called")
+		fmt.Println("subcmdtest called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(connTestCmd)
+	connTestCmd.AddCommand(subcmdtestCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// connTestCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// subcmdtestCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// connTestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// subcmdtestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
